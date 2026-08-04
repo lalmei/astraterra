66b4c08f9b53f42a1ada327c15c4a937e588bc75
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2026-08-03 19:10:21 -0700

    ✨ catalog: include all magnitude-six stars.

--- src/AstraTerra/AstraTerraModMetadata.cs
+++ src/AstraTerra/AstraTerraModMetadata.cs
@@ -2,7 +2,7 @@ namespace AstraTerra;
 
 public static class AstraTerraModMetadata
 {
-    public const string Version = "0.1.49";
+    public const string Version = "0.1.50";
 
     public static string StartupLogMessage => $"AstraTerra mod loaded: version={Version}";
 }
