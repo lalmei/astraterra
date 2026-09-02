c36e2ed02ce7702d30a8f8ffc68aa381aead4fcd
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2026-09-01 17:41:29 -0700

    🔖 release: bump AstraTerra to v0.9.0.

--- src/AstraTerra/AstraTerraModMetadata.cs
+++ src/AstraTerra/AstraTerraModMetadata.cs
@@ -2,7 +2,7 @@ namespace AstraTerra;
 
 public static class AstraTerraModMetadata
 {
-    public const string Version = "0.8.0";
+    public const string Version = "0.9.0";
 
     public static string StartupLogMessage => $"AstraTerra mod loaded: version={Version}";
 }
